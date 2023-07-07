6c9b5fb731579aa54947375dc5620585d5b981b0
Dongseong <hpdh12@me.com>
2023-07-07 10:59:21 +0900

    [feature] driver: read: implement to read 5 times

--- DeviceDriver-Test1/test.cpp
+++ DeviceDriver-Test1/test.cpp
@@ -2,7 +2,22 @@
 #include "gmock/gmock.h"
 #include "../DeviceDriver/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+
+class FlashMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(DeviceDriverTest, CheckDriverRead5Times) {
+	FlashMemoryDeviceMock mockDevice;
+	DeviceDriver driver(&mockDevice);
+
+	EXPECT_CALL(mockDevice, read)
+		.Times(5)
+		.WillRepeatedly(Return(0));
+
+	driver.read(0x4);
 }
